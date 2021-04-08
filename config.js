9fce9ffdcf678ea941aebe9a9ad3ae7460ebc884
Kanishak08192 <kanishak2007@gmail.com>
2021-04-08 18:38:39 +0530

    commit2

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 import * as firebase from 'firebase'
 require('@firebase/firestore')
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCGN_oiYNuKjwXgcL3T74ZivilTTh7AT0",
-  authDomain: "story-hub-12cf6.firebaseapp.com",
-  databaseURL: "https://story-hub-12cf6.firebaseio.com",
-  projectId: "story-hub-12cf6",
-  storageBucket: "story-hub-12cf6.appspot.com",
-  messagingSenderId: "596560767995",
-  appId: "1:596560767995:web:a2a3c10e5e99e504aef95e"
+  apiKey: "AIzaSyC9GV_BQtV6F3kdWjBDzF1RYg4f_g2vl2s",
+  authDomain: "wireless-buzzer-app-a1801.firebaseapp.com",
+  databaseURL: "https://wireless-buzzer-app-a1801.firebaseio.com",
+  projectId: "wireless-buzzer-app-a1801",
+  storageBucket: "wireless-buzzer-app-a1801.appspot.com",
+  messagingSenderId: "512601804012",
+  appId: "1:512601804012:web:69b4763d95c7d4dc3ce9c9",
+  measurementId: "G-HHHECXWLZT"
 };
  
   // Initialize Firebase
